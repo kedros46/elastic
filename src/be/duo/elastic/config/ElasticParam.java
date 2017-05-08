edae07870c390b6905ad533f373aada7a0443ea7
Brech <Brech@192.168.0.143>
2017-05-08 23:31:05 +0200

    [8/05] test mvc

--- src/be/duo/elastic/config/ElasticParam.java
+++ src/be/duo/elastic/config/ElasticParam.java
@@ -3,6 +3,16 @@ package be.duo.elastic.config;
 public enum ElasticParam {
 
 	CLUSTERNAME("cluster.name"),
+	NODENAME("node.name"),
+	NODEATTRRACK("node.attr.rack"),
+	PATHDATA("path.data"),
+	PATHLOGS("pth.logs"),
+	NETWORKHOST("network.host"),
+	NETWORKPORt("network.port"),
+	MEMORYLOCK("bootstrap.memory.lock"),
+	DISCOVERYHOSTS("discovery.zen.ping.unicast.hosts"),
+	DISCOVERYNODES("discovery.zen.minimum_master_nodes"),
+	GATEWAYNODES("gateway.recover_after_nodes"),
 	ISIGNORECLUSTERNAME("client.transport.ignore_cluster_name"),
 	PINGTIMEOUT("client.transport.ping_timeout"),
 	SAMPLERINTERVAL("client.transport.nodes_sampler_interval"),
--- src/be/duo/elastic/controller/ElasticsearchController.java
+++ src/be/duo/elastic/controller/ElasticsearchController.java
@@ -1,14 +1,18 @@
 package be.duo.elastic.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class ElasticsearchController {
 
 	@RequestMapping("/indexing")
 	public ModelAndView index(){
 		String message = "<br><div style='text-align:center;'>"
 				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		System.out.println(message);
+		System.out.println(message);
 		return new ModelAndView("welcome", "message", message);
 	}
 }
